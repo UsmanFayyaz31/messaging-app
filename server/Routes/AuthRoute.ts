f4aa1185cec503dd9feb810b4285a13713c16437
usmanfayyaz31 <usman.fayyaz.3131@gmail.com>
2023-07-25 13:28:58 +0500

    convert async to then

--- server/Routes/AuthRoute.ts
+++ server/Routes/AuthRoute.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 
 import { Login, Signup } from "../Controllers/AuthController";
-import { userVerification } from "../Middlewares/AuthMiddleware";
 
 const router: Router = Router();
 
 router.post("/signup", Signup);
 router.post("/login", Login);
-router.post("/", userVerification);
 
 export default router;
